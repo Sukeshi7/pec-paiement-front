7002922f0d631108f30710cef9489450b92b6a41
oliveade <olive.ade@eemi.com>
2025-07-22 13:24:53 +0200

    pour declencher vercel

--- src/views/ActivationSuccessView.vue
+++ src/views/ActivationSuccessView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-10 text-center">
     <h1 class="text-2xl font-bold text-green-600">✅ Compte activé avec succès !</h1>
-    <p class="mt-4 text-gray-700">Vous pouvez maintenant vous connecter à la plateforme avec vos identifiants.Ils vous ont été envoyés par mail</p>
+    <p class="mt-4 text-gray-700">Vous pouvez desormais vous connecter à la plateforme avec vos identifiants.Ils vous ont été envoyés par mail</p>
     <router-link to="/login" class="mt-6 inline-block text-blue-600 underline">Se connecter</router-link>
   </div>
 </template>
